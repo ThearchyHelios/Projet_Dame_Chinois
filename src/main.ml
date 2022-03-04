fb3e42166a68cbd3c97f9ad6697e349fea8d443e
Marshellson <jiangyilun2000@icloud.com>
2022-03-04 18:35:49 +0100

    Commit

--- src/main.ml
+++ src/main.ml
@@ -1,14 +1,37 @@
 type cordonnee = int * int * int;;
-let dedans (a:cordonnee):bool = 
+type dimension = int;;
+let dimension = 3;;
+
+let check_dimension dimension = match dimension with
+| _ when dimension < 0 -> false
+| _ when dimension > 0 -> true
+| _ -> false;;
+
+(* check_dimension (-4);; *)
+let dedans (a:cordonnee) dimension :bool =
   let x, y, z = a in
   if (x + y + z) != 0 then
     false
   else
-    if (x <= 3 && x >= -3) && (y <= 3 && y >= -3) then true
+    if (x <= dimension && x >= -dimension) && (y <= dimension && y >= -dimension) then true
     else
-      if (y <= 3 && y >= -3) && (z <= 3 && z >= -3) then true
+      if (y <= dimension && y >= -dimension) && (z <= dimension && z >= -dimension) then true
       else
-        if (x <= 3 && x >= -3) && (z <= 3 && z >= -3) then true
+        if (x <= dimension && x >= -dimension) && (z <= dimension && z >= -dimension) then true
         else
           false
         ;;
+
+
+if check_dimension dimension then
+  let a = (0, 0, 0) in
+  let b = dimension in
+  dedans a b
+else
+  false;;
+
+dedans (0, 0, 0) dimension;;
+
+
+(* type couleur = Vert | Rouge | Bleu | Jaune | Marron | Noir;; *)
+
