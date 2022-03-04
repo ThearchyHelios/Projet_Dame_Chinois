f1ec7da0b781878c5f6308fa8c7f0b7940001183
Marshellson <jiangyilun2000@icloud.com>
2022-03-04 19:58:57 +0100

    Bug fixed

--- src/main.ml
+++ src/main.ml
@@ -22,7 +22,7 @@ let est_dans_etoile (a:cordonnee) dimension :bool =
           false
         ;;
 
-
+(* Verifier si la dimension est correct, si oui fonctionner le fonction <est_dans_etoile>, si non (ex: dimension = -3 ou dimension = 0) return false *)
 if check_dimension dimension then
   let a = (0, 0, 0) in
   let b = dimension in
@@ -36,15 +36,18 @@ est_dans_etoile (0, 0, 0) dimension;;
 (* type couleur = Vert | Rouge | Bleu | Jaune | Marron | Noir;; *)
 
 let check_direction cordonnee =
+  let dim = dimension in
   match cordonnee with
-  | (x, y, z) when x > 0 && y < 0 && z < 0 -> "Nord"
-  | (x, y, z) when x > 0 && y > 0 && z < 0 -> "Nord_Ouest"
-  | (x, y, z) when x < 0 && y > 0 && z < 0 -> "Sud_Ouest"
-  | (x, y, z) when x < 0 && y > 0 && z > 0 -> "Sud"
-  | (x, y, z) when x < 0 && y < 0 && z > 0 -> "Sud_Est"
-  | (x, y, z) when x > 0 && y < 0 && z > 0 -> "Nord_Est"
+  | (x, y, z) when x > dim && y < 0 && z < 0 -> "Nord"
+  | (x, y, z) when x > 0 && y > 0 && z < -dim -> "Nord_Ouest"
+  | (x, y, z) when x < 0 && y > dim && z < 0 -> "Sud_Ouest"
+  | (x, y, z) when x < -dim && y > 0 && z > 0 -> "Sud"
+  | (x, y, z) when x < 0 && y < 0 && z > dim -> "Sud_Est"
+  | (x, y, z) when x > 0 && y < -dim && z > 0 -> "Nord_Est"
   | (x, y, z) when x = 0 && y = 0 && z = 0 -> "Point Centre"
   | _ -> "Zone Centrale";;
+
+check_direction (0, -3, 3);;
 
 (* Question 2 *)
 let a:cordonnee = (0, 0, 0) in
