57410ce9ad37f03c790fe89db10cb3272800b9b9
Marshellson <jiangyilun2000@icloud.com>
2022-03-04 19:36:59 +0100

    function check_direction

--- src/main.ml
+++ src/main.ml
@@ -35,3 +35,14 @@ est_dans_etoile (0, 0, 0) dimension;;
 
 (* type couleur = Vert | Rouge | Bleu | Jaune | Marron | Noir;; *)
 
+let check_direction cordonnee =
+  match cordonnee with
+  | (x, y, z) when x > 0 && y < 0 && z < 0 -> "Nord"
+  | (x, y, z) when x > 0 && y > 0 && z < 0 -> "Nord_Ouest"
+  | (x, y, z) when x < 0 && y > 0 && z < 0 -> "Sud_Ouest"
+  | (x, y, z) when x < 0 && y > 0 && z > 0 -> "Sud"
+  | (x, y, z) when x < 0 && y < 0 && z > 0 -> "Sud_Est"
+  | (x, y, z) when x > 0 && y < 0 && z > 0 -> "Nord_Est"
+  | (x, y, z) when x = 0 && y = 0 && z = 0 -> "Point Centre"
+  | _ -> "Zone Centrale";;
+
